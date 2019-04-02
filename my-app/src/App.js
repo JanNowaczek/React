7aeec5315a7d5d3bdf98baafd2ff00fc69fd0d86
Jan Nowaczek <jasiek19_85@o2.pl>
2019-04-02 11:18:23 +0200

    calculator App

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Calculator from 'calculator'
+
+const App = () => {
+  return (
+    <div>
+      <Calculator
+        a={5}
+        b={11}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/Divide.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Divide = (props) => (
+    <div>
+        Ich iloraz to: {props.a / props.b}
+    </div>
+)
+
+export default Divide
